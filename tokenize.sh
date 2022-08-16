1600ec39ed4dcc224f81716d97f8813258a1d104
Ken Sible <ksible@outlook.com>
2022-08-16 15:59:25 -0400

    added separate validation data

--- tokenize.sh
+++ tokenize.sh
@@ -3,20 +3,22 @@
 src_lang=$1
 tgt_lang=$2
 
-mkdir -p data data/train
+mkdir -p data data/train data/dev data/test
 
 echo "Downloading Europarl v7 Training Corpus..."
 wget -q -O data/train.tgz "http://www.statmt.org/europarl/v7/$src_lang-$tgt_lang.tgz" --show-progress
-tar -xvzf data/train.tgz -C data && rm data/train.tgz
+tar -xzf data/train.tgz -C data && rm data/train.tgz
 mv "data/europarl-v7.$src_lang-$tgt_lang.$src_lang" "data/train.$src_lang"
 mv "data/europarl-v7.$src_lang-$tgt_lang.$tgt_lang" "data/train.$tgt_lang"
 
-echo -e "\nDownloading WMT16 Test Set..."
-wget -q -O data/test.tgz "http://data.statmt.org/wmt16/translation-task/test.tgz" --show-progress
-tar -xvzf data/test.tgz -C data && rm data/test.tgz
-find data/test -type f ! -name "*$src_lang$tgt_lang*" -delete
-mv "data/test/newstest2016-$src_lang$tgt_lang-src.$src_lang.sgm" "data/test/test.$src_lang.sgm"
-mv "data/test/newstest2016-$src_lang$tgt_lang-ref.$tgt_lang.sgm" "data/test/test.$tgt_lang.sgm"
+echo -e "\nDownloading WMT 2016 Dev/Test Data..."
+wget -q -O data/dev.tgz "http://data.statmt.org/wmt16/translation-task/dev.tgz" --show-progress
+tar -xzf data/dev.tgz -C data && rm data/dev.tgz
+find data/dev -type f ! -name "*$src_lang$tgt_lang*" -delete
+mv "data/dev/newstest2014-$src_lang$tgt_lang-src.$src_lang.sgm" "data/dev/dev.$src_lang.sgm"
+mv "data/dev/newstest2014-$src_lang$tgt_lang-ref.$tgt_lang.sgm" "data/dev/dev.$tgt_lang.sgm"
+mv "data/dev/newstest2015-$src_lang$tgt_lang-src.$src_lang.sgm" "data/test/test.$src_lang.sgm"
+mv "data/dev/newstest2015-$src_lang$tgt_lang-ref.$tgt_lang.sgm" "data/test/test.$tgt_lang.sgm"
 function sgm_to_txt {
     output="$(python - << END
 import re
@@ -31,23 +33,29 @@ END
 )"
     echo "$output\n"
 }
+sgm_to_txt "data/dev/dev.$src_lang.sgm" > "data/dev/dev.$src_lang"
+sgm_to_txt "data/dev/dev.$tgt_lang.sgm" > "data/dev/dev.$tgt_lang"
 sgm_to_txt "data/test/test.$src_lang.sgm" > "data/test/test.$src_lang"
 sgm_to_txt "data/test/test.$tgt_lang.sgm" > "data/test/test.$tgt_lang"
-rm "data/test/test.$src_lang.sgm" "data/test/test.$tgt_lang.sgm"
+rm data/dev/*.sgm data/test/*.sgm
 
 echo -e "\nPerforming Tokenization with Moses..."
 sacremoses -l $src_lang -j 4 tokenize < "data/train.$src_lang" > "data/train.tok.$src_lang"
 sacremoses -l $tgt_lang -j 4 tokenize < "data/train.$tgt_lang" > "data/train.tok.$tgt_lang"
+sacremoses -l $src_lang -j 4 tokenize < "data/dev/dev.$src_lang" > "data/dev/dev.tok.$src_lang"
+sacremoses -l $tgt_lang -j 4 tokenize < "data/dev/dev.$tgt_lang" > "data/dev/dev.tok.$tgt_lang"
 sacremoses -l $src_lang -j 4 tokenize < "data/test/test.$src_lang" > "data/test/test.tok.$src_lang"
 sacremoses -l $tgt_lang -j 4 tokenize < "data/test/test.$tgt_lang" > "data/test/test.tok.$tgt_lang"
 
 echo -e "\nPerforming Tokenization with BPE..."
-cat "data/train.tok.$src_lang" "data/train.tok.$tgt_lang" | subword-nmt learn-bpe -s 32000 -o "data/bpe.$src_lang$tgt_lang"
-ln -s "bpe.$src_lang$tgt_lang" "data/bpe.$tgt_lang$src_lang"
-subword-nmt apply-bpe -c "data/bpe.$src_lang$tgt_lang" < "data/train.tok.$src_lang" > "data/train.tok.bpe.$src_lang"
-subword-nmt apply-bpe -c "data/bpe.$src_lang$tgt_lang" < "data/train.tok.$tgt_lang" > "data/train.tok.bpe.$tgt_lang"
-subword-nmt apply-bpe -c "data/bpe.$src_lang$tgt_lang" < "data/test/test.tok.$src_lang" > "data/test/test.tok.bpe.$src_lang"
-subword-nmt apply-bpe -c "data/bpe.$src_lang$tgt_lang" < "data/test/test.tok.$tgt_lang" > "data/test/test.tok.bpe.$tgt_lang"
+cat "data/train.tok.$src_lang" "data/train.tok.$tgt_lang" | subword-nmt learn-bpe -s 32000 -o "data/codes.$src_lang$tgt_lang"
+ln -s "codes.$src_lang$tgt_lang" "data/codes.$tgt_lang$src_lang"
+subword-nmt apply-bpe -c "data/codes.$src_lang$tgt_lang" < "data/train.tok.$src_lang" > "data/train.tok.bpe.$src_lang"
+subword-nmt apply-bpe -c "data/codes.$src_lang$tgt_lang" < "data/train.tok.$tgt_lang" > "data/train.tok.bpe.$tgt_lang"
+subword-nmt apply-bpe -c "data/codes.$src_lang$tgt_lang" < "data/dev/dev.tok.$src_lang" > "data/dev/dev.tok.bpe.$src_lang"
+subword-nmt apply-bpe -c "data/codes.$src_lang$tgt_lang" < "data/dev/dev.tok.$tgt_lang" > "data/dev/dev.tok.bpe.$tgt_lang"
+subword-nmt apply-bpe -c "data/codes.$src_lang$tgt_lang" < "data/test/test.tok.$src_lang" > "data/test/test.tok.bpe.$src_lang"
+subword-nmt apply-bpe -c "data/codes.$src_lang$tgt_lang" < "data/test/test.tok.$tgt_lang" > "data/test/test.tok.bpe.$tgt_lang"
 
 echo -e "\nExtracting Shared Vocab with BPE..."
 cat "data/train.tok.bpe.$src_lang" "data/train.tok.bpe.$tgt_lang" | subword-nmt get-vocab > "data/vocab.$src_lang$tgt_lang"
@@ -59,6 +67,9 @@ paste "data/train.tok.bpe.$src_lang" "data/train.tok.bpe.$tgt_lang" > "data/trai
 paste "data/train.tok.bpe.$tgt_lang" "data/train.tok.bpe.$src_lang" > "data/train.tok.bpe.$tgt_lang$src_lang"
 mv data/train.* data/train
 wc -l "data/train/train.tok.bpe.$src_lang$tgt_lang"
+paste "data/dev/dev.tok.bpe.$src_lang" "data/dev/dev.tok.bpe.$tgt_lang" > "data/dev/dev.tok.bpe.$src_lang$tgt_lang"
+paste "data/dev/dev.tok.bpe.$tgt_lang" "data/dev/dev.tok.bpe.$src_lang" > "data/dev/dev.tok.bpe.$tgt_lang$src_lang"
+wc -l "data/dev/dev.tok.bpe.$src_lang$tgt_lang"
 paste "data/test/test.tok.bpe.$src_lang" "data/test/test.tok.bpe.$tgt_lang" > "data/test/test.tok.bpe.$src_lang$tgt_lang"
 paste "data/test/test.tok.bpe.$tgt_lang" "data/test/test.tok.bpe.$src_lang" > "data/test/test.tok.bpe.$tgt_lang$src_lang"
 wc -l "data/test/test.tok.bpe.$src_lang$tgt_lang"

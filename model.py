142c18e7a8bcf53a32cbfd46b3568090678bb48a
Ken Sible <ksible@outlook.com>
2023-05-31 15:47:45 -0400

    added gated residual connections to improve information flow

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 from typing import Callable
 
+import torch
 import torch.nn as nn
 from torch import Tensor
 
@@ -31,10 +32,14 @@ class EncoderLayer(nn.Module):
         self.self_attn = MultiHeadAttention(embed_dim, num_heads, dropout)
         self.ff = FeedForward(embed_dim, ff_dim, dropout)
         self.sublayers = clone(SublayerConnection(embed_dim, dropout), 2)
+        self.gate = nn.Parameter(torch.empty(1))
+        nn.init.normal_(self.gate, mean=1.0, std=0.01)
 
-    def forward(self, src_encs: Tensor, src_mask: Tensor | None = None) -> Tensor:
-        src_encs = self.sublayers[0](src_encs, lambda x: self.self_attn(x, x, x, src_mask))
-        return self.sublayers[1](src_encs, self.ff)
+    def forward(self, src_embs: Tensor, src_encs: Tensor, src_mask: Tensor | None = None) -> Tensor:
+        src_encs = self.gate * src_embs + self.sublayers[0](
+            src_encs, lambda x: self.self_attn(x, x, x, src_mask)
+        )
+        return self.gate * src_embs + self.sublayers[1](src_encs, self.ff)
 
 
 class Encoder(nn.Module):
@@ -47,11 +52,15 @@ class Encoder(nn.Module):
             if p.dim() > 1:
                 nn.init.xavier_uniform_(p)
         self.norm = ScaleNorm(embed_dim**0.5)
+        self.rgate = nn.Parameter(torch.empty(1))
+        self.bgate = nn.Parameter(torch.empty(1))
+        nn.init.normal_(self.rgate, mean=1.0, std=0.01)
+        nn.init.normal_(self.bgate, mean=1.0, std=0.01)
 
     def forward(self, src_embs: Tensor, src_mask: Tensor | None = None) -> Tensor:
         src_encs = src_embs
         for layer in self.layers:
-            src_encs = layer(src_encs, src_mask)
+            src_encs = self.rgate * src_encs + self.bgate * layer(src_embs, src_encs, src_mask)
         return self.norm(src_encs)
 
 
@@ -62,18 +71,25 @@ class DecoderLayer(nn.Module):
         self.crss_attn = MultiHeadAttention(embed_dim, num_heads, dropout)
         self.ff = FeedForward(embed_dim, ff_dim, dropout)
         self.sublayers = clone(SublayerConnection(embed_dim, dropout), 3)
+        self.gate = nn.Parameter(torch.empty(1))
+        nn.init.normal_(self.gate, mean=1.0, std=0.01)
 
     def forward(
         self,
+        tgt_embs: Tensor,
         src_encs: Tensor,
         tgt_encs: Tensor,
         src_mask: Tensor | None = None,
         tgt_mask: Tensor | None = None,
     ) -> Tensor:
         m = src_encs
-        tgt_encs = self.sublayers[0](tgt_encs, lambda x: self.self_attn(x, x, x, tgt_mask))
-        tgt_encs = self.sublayers[1](tgt_encs, lambda x: self.crss_attn(x, m, m, src_mask))
-        return self.sublayers[2](tgt_encs, self.ff)
+        tgt_encs = self.gate * tgt_embs + self.sublayers[0](
+            tgt_encs, lambda x: self.self_attn(x, x, x, tgt_mask)
+        )
+        tgt_encs = self.gate * tgt_embs + self.sublayers[1](
+            tgt_encs, lambda x: self.crss_attn(x, m, m, src_mask)
+        )
+        return self.gate * tgt_embs + self.sublayers[2](tgt_encs, self.ff)
 
 
 class Decoder(nn.Module):
@@ -86,6 +102,10 @@ class Decoder(nn.Module):
             if p.dim() > 1:
                 nn.init.xavier_uniform_(p)
         self.norm = ScaleNorm(embed_dim**0.5)
+        self.rgate = nn.Parameter(torch.empty(1))
+        self.bgate = nn.Parameter(torch.empty(1))
+        nn.init.normal_(self.rgate, mean=1.0, std=0.01)
+        nn.init.normal_(self.bgate, mean=1.0, std=0.01)
 
     def forward(
         self,
@@ -96,7 +116,9 @@ class Decoder(nn.Module):
     ) -> Tensor:
         tgt_encs = tgt_embs
         for layer in self.layers:
-            tgt_encs = layer(src_encs, tgt_encs, src_mask, tgt_mask)
+            tgt_encs = self.rgate * tgt_encs + self.bgate * layer(
+                tgt_embs, src_encs, tgt_encs, src_mask, tgt_mask
+            )
         return self.norm(tgt_encs)
 
 
@@ -140,4 +162,5 @@ class Model(nn.Module):
     ) -> Tensor:
         src_encs = self.encode(src_nums, src_mask)
         tgt_encs = self.decode(src_encs, tgt_nums, src_mask, tgt_mask)
+        return self.out_embed(tgt_encs, inverse=True)
         return self.out_embed(tgt_encs, inverse=True)
